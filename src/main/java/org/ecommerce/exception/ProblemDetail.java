f4bcdb091d2bc467c6a7cb85034f46661abc92d3
Nikos Diakonis <nick.diakonis@gmail.com>
2026-07-12 11:31:17 +0300

    docs(exception): note rfc 9457 stadard in ProblemDetail

--- src/main/java/org/ecommerce/exception/ProblemDetail.java
+++ src/main/java/org/ecommerce/exception/ProblemDetail.java
@@ -1,3 +1,4 @@
 package org.ecommerce.exception;
 
+//Follows RFC 9457 (Problem Details for HTTP APIs) — ex 7807
 public record ProblemDetail(String type, String title, int status, String detail) {}
